89a184a52f27d93602e55c1927b3956d487d6c15
prakash <prakash@rpntechnologies.com>
2020-07-05 21:23:41 +0530

    refactor and pass for 2 numbers

--- StringCalculator/src/main/java/com/StringCalculator/App.java
+++ StringCalculator/src/main/java/com/StringCalculator/App.java
@@ -7,8 +7,12 @@ public class App
 	public int add(String number) {
 		if(strisempty(number)) {
 			return 0;
+		}else if(number.contains(",")) {
+			String[] num = number.split(",");
+			return strtoint(num[0])+ strtoint(num[1]) ;
+		}else {
+			return strtoint(number);
 		}
-		return strtoint(number) ;
 	}
 	
 	private boolean strisempty(String num) {
